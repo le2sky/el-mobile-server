3e17d2f11162ecffb5c179d744873bfba381ae21
leesky <leehaneul0623@gmail.com>
2022-08-05 16:07:38 +0900

    feat: create seeds

--- src/database/seeder/trainer.seeder.ts
+++ src/database/seeder/trainer.seeder.ts
@@ -8,6 +8,9 @@ import { ConfigService } from '@nestjs/config';
 (async () => {
   const app = await NestFactory.createApplicationContext(AppModule);
   const config = app.get(ConfigService);
+  const seeds = Array.from({ length: 20 }, (v, i) => i + 1).map((v, index) => {
+    return new TrainerEntity();
+  });
 
   const AppDataSource = new DataSource({
     type: 'mysql',
