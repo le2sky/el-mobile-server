91b67680c15f70551c736a0d83c38605f1d54f22
leesky <leehaneul0623@gmail.com>
2022-10-01 21:13:46 +0900

    fix: 데이터베이스 약엔티티에서 자체 식별자 추가

--- src/database/entities/feedback.entity.ts
+++ src/database/entities/feedback.entity.ts
@@ -1,9 +1,22 @@
-import { Column, Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 import { HistoryEntity } from './history.entity';
 import { DateAuditEntity } from './interface/dateAudit.entity';
 
 @Entity('feedback')
 export class FeedBackEntity extends DateAuditEntity {
+  @PrimaryGeneratedColumn()
+  feedback_id: number;
+
+  @PrimaryColumn()
+  history_id: number;
+
   @PrimaryColumn()
   customer_id: number;
 
@@ -12,6 +25,7 @@ export class FeedBackEntity extends DateAuditEntity {
 
   @ManyToOne(() => HistoryEntity)
   @JoinColumn([
+    { name: 'history_id', referencedColumnName: 'history_id' },
     { name: 'trainer_id', referencedColumnName: 'trainer_id' },
     { name: 'customer_id', referencedColumnName: 'customer_id' },
   ])
--- src/database/entities/history.entity.ts
+++ src/database/entities/history.entity.ts
@@ -6,6 +6,7 @@ import {
   ManyToOne,
   OneToOne,
   PrimaryColumn,
+  PrimaryGeneratedColumn,
 } from 'typeorm';
 import { DateAuditEntity } from './interface/dateAudit.entity';
 import { MediaEntity } from './media.entity';
@@ -14,6 +15,9 @@ import { TrainingEntity } from './training.entity';
 export const HISOTRY_TYPES = ['workout', 'diet'];
 @Entity('history')
 export class HistoryEntity extends DateAuditEntity {
+  @PrimaryGeneratedColumn()
+  history_id: number;
+
   @PrimaryColumn()
   customer_id: number;
 
